685292d9dba34e8e2194d4f8955b2201f30c1fe8
劉士豪 <st930112@gmail.com>
2016-08-30 22:57:41 +0800

    加入polyline測試、直線變曲線

--- src/Model/Path/Path.js
+++ src/Model/Path/Path.js
@@ -1,19 +1,30 @@
-import Point from '../Point.js';
-import {LineCommand, CurveCommand, Command} from './Command.js';
-
+import {LineCommand, CurveCommand} from './Command.js';
+/**
+ * 描述貝茲路徑的Model
+ */
 export default class Path {
-	//commandString: Line or Curve
+	/*
+	 * @param {Point} [p1] 端點
+	 * @param {Point} [p2] 端點
+	 * @param {string} [commandString] 建立Line還是Curve
+	 */
 	constructor(p1, p2, commandString){
-		if (!(p1 instanceof Point)
-			|| !(p2 instanceof Point)
-			|| !(typeof commandString === 'string')) {
-			console.error('Path init error');
-		}
-
 		this.points = [p1, p2];
 		this.commands = [this.makeCommand(commandString)];
 	}
-
+	appendLineWithPoint(p) {
+		this.points.push(p);
+		this.commands.push(new LineCommand());
+	}
+	/**
+	 * @curveCommand {Command} p L p => p C p  更換頂點間的Command
+	 */
+	makeCurveWithCommandAtIndex(curveCommand, index) {
+		this.commands[index] = curveCommand;
+	}
+	/**
+	 * @private
+	 */
 	makeCommand(commandString){
 		if (commandString === 'Line') return new LineCommand();
 		else if(commandString === 'Curve') return new CurveCommand();
--- test/test_Path.js
+++ test/test_Path.js
@@ -27,6 +27,106 @@ test('make a Line', t=> {
 });
 
 test('append LineCommand', t=>{
-	let test_path = new Path(p1, p2, 'Line');
-	test.appendLine();
+	let test_path = new Path(p1, p2, 'Line'),
+		p3 = new Point(30, 10);
+
+	test_path.appendLineWithPoint(p3);
+
+	/*
+	三個點 兩個Line Command
+	*	[p1, p2, p3]
+		[  L,  L   ]
+	*/
+	t.true(test_path.points.length === 3);
+	t.true(test_path.commands.length === 2);
+
+	t.true(test_path.points[0] === p1 );
+	t.true(test_path.points[1] === p2 );
+	t.true(test_path.points[2] === p3 );
+	t.true( test_path.commands[0] instanceof LineCommand);
+	t.true( test_path.commands[1] instanceof LineCommand);
+});
+
+test('make polyline with random points', t=>{
+	let pointArray = [p1, p2],
+		linePath = new Path(p1, p2, 'Line');
+	for (let i = 0; i < 10; i++) {
+		let p = makeRandomPoint();
+		pointArray.push(p);
+		linePath.appendLineWithPoint(p);
+	}
+	for (let i = 0; i < pointArray.length; i++) {
+		t.true( linePath.points[i] === pointArray[i] );
+		if (i > 0) {
+			t.true( linePath.commands[i-1] instanceof LineCommand );
+		}
+	}
+
+	function makeRandomPoint() {
+		let x = oneToTen(),
+			y = oneToTen();
+
+		return new Point(x, y);
+	}
+	function oneToTen() {
+		return Math.floor((Math.random() * 10) + 1);
+	}
+});
+
+test('make line to curve', t=>{
+	//p L p L p => p C p L p
+	//p: 頂點 C:貝茲控制指令
+	//預設建立於線上四分之一處
+
+	let test_path = new Path(p1, p2, 'Line'),
+		p3 = new Point(30, 10);
+	test_path.appendLineWithPoint(p3);
+
+
+	let c1 = makeControlPoint(p1, p2),
+		c2 = makeControlPoint(p2, p1),
+		curve = new CurveCommand(c1, c2);
+	test_path.makeCurveWithCommandAtIndex(curve, 0);
+
+	t.true( test_path.commands.length === 2);
+	t.true( test_path.commands[0] instanceof CurveCommand);
+	t.true( test_path.commands[1] instanceof LineCommand);
+
+	function makeControlPoint(p1, p2) {
+		return new Point(quartile(p1.x, p2.x),
+				quartile(p1.y, p2.y));
+	}
+	// 1/4
+	function quartile(a, b) {
+		return (a + 3*b) /4;
+	}
+});
+
+test('make curve to line', t=>{
+	//p L p L p => p C p L p
+	//p: 頂點 C:貝茲控制指令
+	//預設建立於線上四分之一處
+
+	let test_path = new Path(p1, p2, 'Line'),
+		p3 = new Point(30, 10);
+	test_path.appendLineWithPoint(p3);
+
+
+	let c1 = makeControlPoint(p1, p2),
+		c2 = makeControlPoint(p2, p1),
+		curve = new CurveCommand(c1, c2);
+	test_path.makeCurveWithCommandAtIndex(curve, 0);
+
+	t.true( test_path.commands.length === 2);
+	t.true( test_path.commands[0] instanceof CurveCommand);
+	t.true( test_path.commands[1] instanceof LineCommand);
+
+	function makeControlPoint(p1, p2) {
+		return new Point(quartile(p1.x, p2.x),
+				quartile(p1.y, p2.y));
+	}
+	// 1/4
+	function quartile(a, b) {
+		return (a + 3*b) /4;
+	}
 });
